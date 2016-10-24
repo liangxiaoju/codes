2c5049dc1eaadf963f59071dc9f5d036778d45ea
liangxiaoju <liangxiaoju@gmail.com>
2016-10-24 13:57:20 +0800

    chess: modify endgame map

--- cocos2dx/projects/chess/Classes/ChallengeMap.cpp
+++ cocos2dx/projects/chess/Classes/ChallengeMap.cpp
@@ -1,20 +1,23 @@
 #include "ChallengeMap.h"
 #include "Utils.h"
 
-bool ChallengeMap::init(int total, int progress)
+bool ChallengeMap::init(int total)
 {
-    if (!Layout::init())
+    if (!ScrollView::init())
         return false;
 
     _clickEventCallback = nullptr;
-    _currentFlag = nullptr;
 
     auto vsize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     auto image = ImageView::create("challenge_map.png");
     auto size = image->getContentSize();
+    image->setScale(vsize.width/size.width);
+    size = size * image->getScale();
     image->setAnchorPoint(Vec2(0, 0));
+
+    _innerLayout = Layout::create();
 
     int i;
     float y = 0;
@@ -22,18 +25,17 @@ bool ChallengeMap::init(int total, int progress)
         auto clone = image->clone();
         clone->setPosition(Vec2(0, y));
         y += size.height;
-        addChild(clone);
+        _innerLayout->addChild(clone);
     }
-
     int num = i;
-    setContentSize(Size(size.width, size.height*num));
+    _innerLayout->setContentSize(Size(size.width, size.height*num));
 
-    float w = 694;
-    float h = 587;
+    float w = 694 * image->getScale();
+    float h = 587 * image->getScale();
 
     for (i = 0; i < num; i++) {
-        float x = 198;
-        float y = 387 + i*size.height;
+        float x = 198 * image->getScale();
+        float y = 387 * image->getScale() + i*size.height;
         _positions.push_back(Vec2(x, y));
         _positions.push_back(Vec2(x+w, y+h));
         _positions.push_back(Vec2(x, y+h*2));
@@ -43,25 +45,35 @@ bool ChallengeMap::init(int total, int progress)
     }
 
     _total = total;
-    setProgress(progress);
+
+    this->setScrollBarEnabled(false);
+    this->setBounceEnabled(false);
+    this->setDirection(ScrollView::Direction::VERTICAL);
+    this->setContentSize(vsize);
+    this->setPosition(Vec2(0, 0));
+
+    this->setInnerContainerSize(_innerLayout->getContentSize());
+    this->addChild(_innerLayout);
+    this->jumpToBottom();
 
     return true;
 }
 
 void ChallengeMap::setProgress(int progress)
 {
-    if (_currentFlag)
-        removeChild(_currentFlag);
+    if (progress < 0 || progress > _total)
+        return;
 
-    for (auto flag : _passFlags)
-        removeChild(flag);
+    for (auto flag : _flags)
+        _innerLayout->removeChild(flag);
+    _flags.clear();
 
     auto addText = [this](int i) {
         std::string str = std::string("No.") + Utils::toString(i+1);
-        auto text = Text::create(str, "", 35);
+        auto text = Text::create(str, "", 30);
         text->setTextColor(Color4B::BLACK);
         text->setPosition(Vec2(_positions[i].x, _positions[i].y-90));
-        addChild(text);
+        _innerLayout->addChild(text);
     };
 
     auto callback = [this](Ref *ref) {
@@ -75,24 +87,23 @@ void ChallengeMap::setProgress(int progress)
     for (int i = 0; i < progress; i++) {
         auto flag = Button::create("break_flag_lv.png");
         flag->setPosition(_positions[i]);
-        addChild(flag);
+        _innerLayout->addChild(flag);
         flag->setTag(i);
         flag->addClickEventListener(callback);
         addText(i);
 
-        _passFlags.push_back(flag);
+        _flags.push_back(flag);
     }
 
     if (progress < _total) {
         auto flag = Button::create("break_flag_red.png");
         flag->setPosition(_positions[progress]);
-        addChild(flag);
+        _innerLayout->addChild(flag);
         flag->setTag(progress);
         flag->addClickEventListener(callback);
-        flag->setName("focus");
         addText(progress);
 
-        _currentFlag = flag;
+        _flags.push_back(flag);
     }
 
     _progress = progress;
@@ -104,3 +115,19 @@ void ChallengeMap::addClickEventListener(
     _clickEventCallback = callback;
 }
 
+void ChallengeMap::jumpToProgress(int progress)
+{
+    auto vsize = Director::getInstance()->getVisibleSize();
+
+    if (progress >= 0 && progress <= _total) {
+        auto pos = _positions[progress];
+        auto size = _innerLayout->getContentSize();
+        float percent = 100 -
+            100*(pos.y-vsize.height/2)/(size.height-vsize.height);
+        percent = std::max(percent, 0.0f);
+        percent = std::min(percent, 100.0f);
+        this->jumpToPercentVertical(percent);
+    } else {
+        this->jumpToBottom();
+    }
+}
--- cocos2dx/projects/chess/Classes/ChallengeMap.h
+++ cocos2dx/projects/chess/Classes/ChallengeMap.h
@@ -7,14 +7,14 @@
 USING_NS_CC;
 using namespace cocos2d::ui;
 
-class ChallengeMap : public Layout
+class ChallengeMap : public ScrollView
 {
 public:
-    virtual bool init(int total, int progress);
-    static ChallengeMap *create(int total, int progress)
+    virtual bool init(int total);
+    static ChallengeMap *create(int total)
     {
         auto pRet = new (std::nothrow) ChallengeMap();
-        if (pRet && pRet->init(total, progress)) {
+        if (pRet && pRet->init(total)) {
             pRet->autorelease();
             return pRet;
         } else {
@@ -26,12 +26,13 @@ public:
     void setProgress(int progress);
     int getProgress() { return _progress; }
     void addClickEventListener(const std::function<void(int)> &callback);
+    void jumpToProgress(int progress);
 
 private:
     std::function<void(int)> _clickEventCallback;
-    std::vector<Button*> _passFlags;
-    Button *_currentFlag;
+    std::vector<Button*> _flags;
     std::vector<Vec2> _positions;
+    Layout *_innerLayout;
     int _total;
     int _progress;
 };
